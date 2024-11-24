ae9964a02adbd030b4d5627071b550d0a0ec5890
Jesse Koldewijn <jesse.koldewijn@justeattakeaway.com>
2024-11-24 16:16:03 +0100

    init: base setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,21 @@
-// @ts-check
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://dulljoker.eu",
+	integrations: [
+		tailwind({
+			configFile: "tailwind.config.ts",
+			applyBaseStyles: false,
+			nesting: true,
+		}),
+		react(),
+		sitemap(),
+	],
+});
---
+++ src/components/branding/name.tsx
@@ -0,0 +1,10 @@
+const BrandingName = () => {
+	return (
+		<>
+			<span className="text-neutral-50 font-mono font-bold">Dull</span>
+			<span className="drop-shadow-lg shadow-neutral-200">Joker</span>
+		</>
+	);
+};
+
+export default BrandingName;
