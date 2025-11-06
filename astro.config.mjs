16d2aae6a580417d83b3f86fe4d1edad8f2a306a
Jesse Koldewijn <jesse.koldewijn@justeattakeaway.com>
2025-11-06 18:02:41 +0100

    feat: ComfyUI docs page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,14 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+import tailwindCss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://dulljoker.eu",
-	integrations: [
-		tailwind({
-			configFile: "tailwind.config.ts",
-			applyBaseStyles: false,
-			nesting: true,
-		}),
-		react(),
-		sitemap(),
-	],
+	integrations: [react(), sitemap()],
+	vite: {
+		plugins: [tailwindCss()],
+	},
 });
